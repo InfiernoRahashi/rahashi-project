1dbc50f6cb8b6d8424c3ae3f48a5d32b46cdedd1
Infierno <73641054+InfiernoRahashi@users.noreply.github.com>
2020-10-29 22:50:17 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+const Discord = require("discord.js");
+const moment = require('moment');
+var ffmpeg = require('ffmpeg');
+ 
+const client = new Discord.Client();
+const config = require("./config.json");
+const package = require("./package.json");
+ const getUserFromMention = (mention, client) => {
+  if (!mention) return;
+  if (mention.startsWith('<@') && mention.endsWith('>')) {
+      mention = mention.slice(2, -1);
+      if (mention.startsWith('!')) {
+          mention = mention.slice(1);
+      }
+      return client.users.cache.get(mention);
+  }
+}
 
+  
+
+client.login(config.token);
